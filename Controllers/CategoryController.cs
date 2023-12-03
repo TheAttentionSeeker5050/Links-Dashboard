1eed19c5473f1d93ae39c10a7c5dfd3ff510ae6a
nicolas <nicolas.castellano.6@hotmail.com>
2023-12-02 23:43:24 -0400

    fixed edit category form

--- Controllers/CategoryController.cs
+++ Controllers/CategoryController.cs
@@ -2,6 +2,10 @@
 using Microsoft.AspNetCore.Mvc;
 using WebAppProject3.Data;
 using WebAppProject3.Models;
+using System.Linq;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 
 namespace WebAppProject3.Controllers
 {
@@ -20,13 +24,58 @@ namespace WebAppProject3.Controllers
 
         // GET: CategoryController
         [HttpGet]
+        [Authorize]
         [Route("/category/{id}")]
-        public ActionResult Edit()
+        public IActionResult Edit()
         {
-            // initiate the category model
-            var category = new CategoryModel();
+            // get the category id from the route
+            var id = RouteData.Values["id"].ToString();
+            var category = _context.Categories.FirstOrDefault(
+                                   c => c.CategoryId.ToString() == id);
 
             return View(category);
+        }
+
+        // POST: CategoryController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        [Route("/category/{id}")]
+        public async Task<IActionResult> EditSubmit(CategoryModel category, int id)
+        {
+            // validate the form
+            if (!ModelState.IsValid)
+            {
+                return View("Edit");
+            }
+
+            try
+            {
+                // find the Category model by id
+                var categoryFromDb = _context.Categories.FirstOrDefault(
+                                                      c => c.CategoryId == id);
+                if (categoryFromDb == null)
+                {
+                    // return error on form
+                    ModelState.AddModelError("Category", "Category not found");
+                    return View("Edit");
+                }
+
+                // update the category name
+                categoryFromDb.CategoryName = category.CategoryName;
+
+                // save changes to database
+                _context.Categories.Update(categoryFromDb);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index", "Home");
+            }
+            catch (System.Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                
+                return View("Edit");
+            }
         }
 
     }
