bb397ea17a133749c6a07dfbe905a761635a07ec
nicolas <nicolas.castellano.6@hotmail.com>
2023-12-13 20:43:20 -0400

    .

--- Program.cs
+++ Program.cs
@@ -40,6 +40,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios.
     app.UseHsts();
+    app.UseHttpsRedirection();
 }
 
 app.UseHttpsRedirection();
